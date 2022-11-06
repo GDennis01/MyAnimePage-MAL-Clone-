023dd2735628cb8c1ad9170483bd54185e97976e
Dennis Gobbi <dennis.gobbi@edu.unito.it>
2022-11-06 02:50:55 +0100

    fixed registrati

--- api/registrati.php
+++ api/registrati.php
@@ -1,25 +1,24 @@
 <?php
 include 'utils.php';
-if(!isset($_POST))
+if(!isset($_POST) || !isset($_POST['username']) || !isset($_POST['password'])){
+    echo json_encode("Inserire username e password");
   return;
-
+}
 $user = $_POST['username'];
-$pass = $_POST['pw'];
+$pass = $_POST['password'];
 $conn = dbConn();
 
 $sql = "SELECT name FROM user WHERE name = '$user'";
 $result = mysqli_query($conn, $sql);
-$user = mysqli_fetch_assoc($result);
 if(mysqli_num_rows($result) == 1){//Se la query restituisce una table con UNA sola riga,vuol dire che ha trovato la corrispondenza
   echo json_encode("Username already taken");
 }else if(mysqli_num_rows($result)== 0){//Se la query restituisce una table con nessuna riga,vuol dire che non ha trovato la corrispondenza
-  $sql = "INSERT INTO user(name,password) values (\"$user\",\"$pass\");";
+  $date = date("Y-m-d");
+  $sql = "INSERT INTO user(name,password,data_creazione) values ('$user','$pass','$date');";
   $conn->query($sql);
   session_start();         
   $_SESSION['id']=$user;
   $_SESSION['logged']=true;
   echo json_encode("success");
 }
-
-
-  ?>
+?>
