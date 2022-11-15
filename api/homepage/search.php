08ad3eddfaaf314595fcf58d813a88d9e24502af
Dennis Gobbi <dennis.gobbi@edu.unito.it>
2022-11-15 21:06:33 +0100

    Cleaner code

--- api/homepage/search.php
+++ api/homepage/search.php
@@ -1,18 +1,18 @@
 <?php
-//TODO:check if the user is logged in 
+
 include "../utils.php";
 $db = dbConn();
-$search=$_GET['search'];
+$search = $_GET['search'];
 $query = "SELECT MAL_ID as id, Name as name FROM anime_list WHERE LOWER(Name) LIKE '%$search%'";
 // mysqli
 $result = mysqli_query($db, $query);
 $json = array();
 if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_assoc($result)) {
-        $json[] = $row;
-    }
+  while ($row = mysqli_fetch_assoc($result)) {
+    $json[] = $row;
+  }
 } else {
-    $json = "";
+  $json = "";
 }
 mysqli_close($db);
 echo json_encode($json);
