ddf71fce8524141b4deb8921427260e2124f9152
Dennis Gobbi <dennis.gobbi@edu.unito.it>
2022-12-01 22:42:15 +0100

    Recreated dbConn with PDO

--- api/utils.php
+++ api/utils.php
@@ -1,10 +1,29 @@
 <?php
 
-function dbConn(){
-    $conn = mysqli_connect("localhost", "root", "", "anime_db");
-    if (!$conn) {
-      die("Connection failed: " . mysqli_connect_error());
-    }
-    return $conn;
+// function dbConn(){
+//     $conn = mysqli_connect("localhost", "root", "", "anime_db");
+//     if (!$conn) {
+//       die("Connection failed: " . mysqli_connect_error());
+//     }
+//     return $conn;
+// }
+function dbConn()
+{
+  // $conn = mysqli_connect("localhost", "root", "", "anime_db");
+  // if (!$conn) {
+  //   die("Connection failed: " . mysqli_connect_error());
+  // }
+
+  //PDO
+  try {
+    $dsn = "mysql:dbname=anime_db;host=localhost";
+    $user = "root";
+    $password = "";
+    $conn = new PDO($dsn, $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+  } catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+    die();
+  }
+  return $conn;
 }
-?>
