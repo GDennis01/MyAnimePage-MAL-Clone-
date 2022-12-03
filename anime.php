ed88b66c249c632c9fe5e4f0b1b19a5ca6091705
Dennis Gobbi <dennis.gobbi@edu.unito.it>
2022-12-03 12:53:21 +0100

    anime.php now uses anime.js

--- anime.php
+++ anime.php
@@ -26,6 +26,7 @@ if ($stmt->rowCount() == 0) {
 <?php include 'templates/header.html' ?>
 <link href="css/animepage.css" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="js/anime.js"></script>
 </head>
 
 <body>
---
+++ js/anime.js
@@ -0,0 +1,70 @@
+/*JS File with anime.php related functions such as 'adding to list an anime' or posting a review*/
+
+/**
+ * Api:anime
+ * @param {*} mal_id id of myanimelist entry in the database
+ * @param {*} id_user id of the user in the user table
+ */
+function addToList(mal_id, id_user) {
+  $.ajax({
+    url: "api/anime/addList.php",
+    type: "POST",
+    data: { mal_id: mal_id, id_user: id_user },
+    dataType: "json",
+    success: function (response) {
+      console.log(response);
+      if (response == "success") {
+        $("#btnAddList").html("Already added to your anime page");
+        $("#btnAddList").attr("disabled", true);
+      } else {
+        $("#login-error").html(response);
+      }
+    }
+  })
+}
+/**
+ * Api:anime
+ * @param {Integer} mal_id 
+ * @param {Integer} id_user 
+ */
+function postReview(mal_id, id_user) {
+  let review = $("#review").val();
+  console.log(review);
+  $.ajax({
+    url: "api/anime/postReview.php",
+    type: "POST",
+    data: { mal_id: mal_id, id_user: id_user, review: review },
+    dataType: "json",
+    success: function (response) {
+      console.log(response);
+      if (response == "success") {
+        location.reload();
+        // $("#review").val("");
+        // $("#review-error").html("Review posted!");
+      } else {
+        $("#review-error").html(response);
+      }
+    }
+  })
+}
+/**
+ * Api:anime
+ * @param {Integer} mal_id
+ * @param {Integer} id_user
+ */
+function deleteReview(id_review) {
+  $.ajax({
+    url: "api/anime/deleteReview.php",
+    type: "POST",
+    data: { id_review: id_review },
+    dataType: "json",
+    success: function (response) {
+      console.log(response);
+      if (response == "success") {
+        location.reload();
+      } else {
+        $("#review-error").html(response);
+      }
+    }
+  })
+}
