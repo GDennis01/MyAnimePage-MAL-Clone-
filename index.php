a506639796978322b49f197aa03b394e0b9cc2c0
Dennis Gobbi <dennis.gobbi@edu.unito.it>
2022-12-03 15:46:42 +0100

    Carousel now working properly(css aside)

--- index.php
+++ index.php
@@ -24,64 +24,72 @@ $watched = $_SESSION['watched'];
   </header>
   <!-- Script tag so JS run as soon as possible -->
   <script>
-    let random = [];
-    //ajax call to generate 3 random mal_id
-    $.ajax({
-      url: 'api/homepage/random.php',
-      type: 'POST',
-      success: function(data) {
-        random.push(data);
-        $.ajax({
-          url: 'api/homepage/random.php',
-          type: 'POST',
-          success: function(data) {
-            random.push(data);
-            $.ajax({
-              url: 'api/homepage/random.php',
-              type: 'POST',
-              success: function(data) {
-                random.push(data);
-                console.log(random);
-                for (let i = 0; i < 3; i++) {
-                  sendApiRequest(random[i], i);
-                }
-              }
-            });
-          }
-        });
-      }
-    });
+    // let random = [];
+    // //ajax call to generate 3 random mal_id
+    // $.ajax({
+    //   url: 'api/homepage/random.php',
+    //   type: 'POST',
+    //   success: function(data) {
+    //     random.push(data);
+    //     $.ajax({
+    //       url: 'api/homepage/random.php',
+    //       type: 'POST',
+    //       success: function(data) {
+    //         random.push(data);
+    //         $.ajax({
+    //           url: 'api/homepage/random.php',
+    //           type: 'POST',
+    //           success: function(data) {
+    //             random.push(data);
+    //             console.log(random);
+    //             for (let i = 0; i < 3; i++) {
+    //               sendApiRequest(random[i], i);
+    //             }
+    //           }
+    //         });
+    //       }
+    //     });
+    //   }
+    // });
   </script>
 
   <!-- NAVBAR -->
   <?php include('templates/navbar.html') ?>
+
+
+  <?php
+  $conn = dbConn();
+  $sql = "SELECT MAL_ID,image,synopsis FROM anime_list ORDER BY RAND() LIMIT 3";
+  $stmt = $conn->prepare($sql);
+  $stmt->execute();
+  $anime_1 = $stmt->fetch(PDO::FETCH_ASSOC);
+  $anime_2 = $stmt->fetch(PDO::FETCH_ASSOC);
+  $anime_3 = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  ?>
   <!-- Carousel -->
   <div id="carouselContainer">
     <div id="sugg_anime">
-      <h3>Top suggested anime!</h3>
+      <h3>Anime of the day!</h3>
     </div>
+
     <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
       <!-- Actual carousel -->
       <!-- ids and img should be replaced when body loads by an ajax function that generates 3 random number from 
           the pool of mal ids in the database -->
+
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <a id="slider_a_0"><img id="slider_0" src="img/notavailable.png" class="w-100" alt="..." width="320" height="240"></a>
+          <a id="slider_a_0" href='anime.php?id=<?= $anime_1['MAL_ID'] ?>'><img id="slider_0" src='<?= $anime_1['image'] ?>' class="w-100" alt="..." width="320" height="240"></a>
         </div>
         <div class="carousel-item">
-          <a id="slider_a_1"><img id="slider_1" src="img/notavailable2.png" class="w-100" alt="..." width="320" height="240"></a>
+          <a id="slider_a_1" href='anime.php?id=<?= $anime_2['MAL_ID'] ?>'><img id="slider_1" src='<?= $anime_2['image'] ?>' class="w-100" alt="..." width="320" height="240"></a>
         </div>
         <div class="carousel-item">
-          <a id="slider_a_2"><img id="slider_2" src="img/notavailable3.png" class="w-100" alt="..." width="320" height="240"></a>
+          <a id="slider_a_2" href='anime.php?id=<?= $anime_3['MAL_ID'] ?>'><img id="slider_2" src='<?= $anime_3['image'] ?>' class="w-100" alt="..." width="320" height="240"></a>
         </div>
       </div>
-      <!-- Current active carousel anime synopsis-->
-      <!--  Search through the dom for carousel-item active, then get its mal id, ajax it to the db to find its
-            Synopsis and innerHTML'd it here -->
-      <div id="carouselSynopsis">
-        <!-- <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum officiis, accusamus optio quos assumenda magnam ut est nemo dignissimos, temporibus numquam libero veritatis rerum odio inventore ipsam beatae, eligendi eveniet. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet ab voluptates et in nulla consectetur amet at error voluptas veniam illum delectus repellendus similique tenetur libero repellat sed, illo minus.</p> -->
-        <p></p>
-      </div>
+
       <!-- Carousel control buttons -->
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
         <span class="carousel-control-prev-icon" aria-hidden="true"></span>
@@ -92,6 +100,13 @@ $watched = $_SESSION['watched'];
         <span class="visually-hidden">Next</span>
       </button>
 
+    </div>
+    <!-- Current active carousel anime synopsis-->
+    <div id="carouselSynopsis">
+      <!-- <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum officiis, accusamus optio quos assumenda magnam ut est nemo dignissimos, temporibus numquam libero veritatis rerum odio inventore ipsam beatae, eligendi eveniet. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet ab voluptates et in nulla consectetur amet at error voluptas veniam illum delectus repellendus similique tenetur libero repellat sed, illo minus.</p> -->
+      <p id="syn0" class="synopsis"><?= $anime_1['synopsis'] ?></p>
+      <p id="syn1" class="synopsis"><?= $anime_2['synopsis'] ?></p>
+      <p id="syn2" class="synopsis"><?= $anime_3['synopsis'] ?></p>
     </div>
   </div>
   <!-- Aside -->
@@ -113,7 +128,7 @@ $watched = $_SESSION['watched'];
     </ul>
 
   </aside>
-  <button id="test_api" onclick="sendApiRequest()">Prova</button>
+  <!-- <button id="test_api" onclick="sendApiRequest()">Prova</button> -->
   <!-- Footer -->
   <?php include('templates/footer.html') ?>
 </body>
--- js/util.js
+++ js/util.js
@@ -2,80 +2,100 @@
 JS File with common functions available through all the pages
 Mainly navbar related js functions
 */
+/*
+Attach an event listener whenever the carousel animation ends
+This is useful so that we can change the synopsis text
+*/
+$(document).ready(function () {
+  //right after the animation starts
+  // $('#carouselExampleControls').on('slide.bs.carousel', function () {
+  //   //alert("Slide Event");
+  //   console.log('slid event');
+  // });
 
-async function sendApiRequest(mal_id, number_div = -1) {
-  // Here we define our query as a multi-line string
-  // Storing it in a separate .graphql/.gql file is also possible
-  var query = `
-query ($id: Int) { # Define which variables will be used in the query (id)
-  Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
-    id
-    title {
-      romaji
-      english
-      native
+  $('#carouselExampleControls').on('slid.bs.carousel', function (event) {
+    for (let i = 0; i < 3; i++) {
+      if (event.to == i) {
+        $("#syn" + i).show();
+      } else
+        $("#syn" + i).hide();
     }
-    coverImage{
-      large
-    }
-  }
-}
-`;
-
-  // Define our query variables and values that will be used in the query request
-  var variables = {
-    id: mal_id
-    // id: 1
-  };
-
-  // Define the config we'll need for our Api request
-  var url = 'https://graphql.anilist.co',
-    options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-      },
-      body: JSON.stringify({
-        query: query,
-        variables: variables
-      })
-    };
-
-
-  // Make the HTTP Api request
-  let _handleData = await fetch(url, options).then(handleResponse)
-    .then(
-      (data) => {
-        // console.log(data);
-        return data;
-      }
-    )
-    .catch(handleError);
-  console.log(_handleData);
-  if (number_div != -1) {
-    $("#slider_" + number_div).attr("src", _handleData.data.Media.coverImage.large);
-    $("#slider_a_" + number_div).attr("href", "anime.php?id=" + _handleData.data.Media.id);
-  }
-  return _handleData;
-
-}
-
-function handleResponse(response) {
-  return response.json().then(function (json) {
-    return response.ok ? json : Promise.reject(json);
   });
-}
+});
 
-function handleData(data) {
-  _handleData = data;
-  // console.log(data);
-}
+// async function sendApiRequest(mal_id, number_div = -1) {
+//   // Here we define our query as a multi-line string
+//   // Storing it in a separate .graphql/.gql file is also possible
+//   var query = `
+// query ($id: Int) { # Define which variables will be used in the query (id)
+//   Media (id: $id, type: ANIME) { # Insert our variables into the query arguments (id) (type: ANIME is hard-coded in the query)
+//     id
+//     title {
+//       romaji
+//       english
+//       native
+//     }
+//     coverImage{
+//       large
+//     }
+//   }
+// }
+// `;
 
-function handleError(error) {
-  // alert('Error, check console');
-  // console.error(error);
-}
+//   // Define our query variables and values that will be used in the query request
+//   var variables = {
+//     id: mal_id
+//     // id: 1
+//   };
+
+//   // Define the config we'll need for our Api request
+//   var url = 'https://graphql.anilist.co',
+//     options = {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Accept': 'application/json',
+//       },
+//       body: JSON.stringify({
+//         query: query,
+//         variables: variables
+//       })
+//     };
+
+
+//   // Make the HTTP Api request
+//   let _handleData = await fetch(url, options).then(handleResponse)
+//     .then(
+//       (data) => {
+//         // console.log(data);
+//         return data;
+//       }
+//     )
+//     .catch(handleError);
+//   console.log(_handleData);
+//   if (number_div != -1) {
+//     $("#slider_" + number_div).attr("src", _handleData.data.Media.coverImage.large);
+//     $("#slider_a_" + number_div).attr("href", "anime.php?id=" + _handleData.data.Media.id);
+//   }
+//   return _handleData;
+
+// }
+
+// function handleResponse(response) {
+//   return response.json().then(function (json) {
+//     return response.ok ? json : Promise.reject(json);
+//   });
+// }
+
+// function handleData(data) {
+//   _handleData = data;
+//   // console.log(data);
+// }
+
+// function handleError(error) {
+//   // alert('Error, check console');
+//   // console.error(error);
+// }
 /**
  * Api:homepage
  */
@@ -124,9 +144,6 @@ function getAjaxRes() {
   }
 }
 
-/**
- * Api:homepage
- */
 function randomAnime() {
   $.ajax({
     url: "api/homepage/random.php",
