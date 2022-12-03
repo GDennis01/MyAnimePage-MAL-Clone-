8cd98db3930634248add73e1c973d1ca6e80cd0f
Dennis Gobbi <dennis.gobbi@edu.unito.it>
2022-12-03 12:26:45 +0100

    Refactored random to pdo

--- api/homepage/random.php
+++ api/homepage/random.php
@@ -9,6 +9,9 @@ if (!isset($_POST))
   return;
 $db = dbConn();
 $query = "SELECT MAL_ID FROM anime_list ORDER BY RAND() LIMIT 1";
-$result = mysqli_query($db, $query);
-$row = mysqli_fetch_assoc($result);
+$stmt = $db->prepare($query);
+$stmt->execute();
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+// $result = mysqli_query($db, $query);
+// $row = mysqli_fetch_assoc($result);
 echo $row['MAL_ID'];
