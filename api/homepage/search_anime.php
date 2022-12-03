0e7d106c8f347253b841b35767abae80339065c0
Dennis Gobbi <dennis.gobbi@edu.unito.it>
2022-12-03 12:26:39 +0100

    Refactored search bar to pdo

--- api/homepage/search_anime.php
+++ api/homepage/search_anime.php
@@ -10,16 +10,22 @@ if (!isset($_POST))
   return;
 $db = dbConn();
 $search = $_GET['search'];
-$query = "SELECT MAL_ID as id, Name as name FROM anime_list WHERE LOWER(Name) LIKE '%$search%'";
+// $query = "SELECT MAL_ID as id, Name as name FROM anime_list WHERE LOWER(Name) LIKE '%$search%'";
+$query = "SELECT MAL_ID as id, Name as name FROM anime_list WHERE LOWER(Name) LIKE ?";
+$stmt = $db->prepare($query);
+$stmt->execute(["%$search%"]);
 // mysqli
-$result = mysqli_query($db, $query);
+// $result = mysqli_query($db, $query);
+
 $json = array();
-if (mysqli_num_rows($result) > 0) {
-  while ($row = mysqli_fetch_assoc($result)) {
+// if (mysqli_num_rows($result) > 0) {
+if ($stmt->rowCount() > 0) {
+  // while ($row = mysqli_fetch_assoc($result)) {
+  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $json[] = $row;
   }
 } else {
   $json = "";
 }
-mysqli_close($db);
+// mysqli_close($db);
 echo json_encode($json);
--- api/homepage/search_user.php
+++ api/homepage/search_user.php
@@ -10,16 +10,21 @@ if (!isset($_POST))
   return;
 $db = dbConn();
 $search = $_GET['search'];
-$query = "SELECT  id_user as id, name FROM user WHERE LOWER(name) LIKE '%$search%'";
+// $query = "SELECT  id_user as id, name FROM user WHERE LOWER(name) LIKE '%$search%'";
+$query = "SELECT  id_user as id, name FROM user WHERE LOWER(name) LIKE ?";
+$stmt = $db->prepare($query);
+$stmt->execute(["%$search%"]);
 // mysqli
-$result = mysqli_query($db, $query);
+// $result = mysqli_query($db, $query);
 $json = array();
-if (mysqli_num_rows($result) > 0) {
-  while ($row = mysqli_fetch_assoc($result)) {
+// if (mysqli_num_rows($result) > 0) {
+if ($stmt->rowCount() > 0) {
+  // while ($row = mysqli_fetch_assoc($result)) {
+  while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $json[] = $row;
   }
 } else {
   $json = "";
 }
-mysqli_close($db);
+// mysqli_close($db)
 echo json_encode($json);
