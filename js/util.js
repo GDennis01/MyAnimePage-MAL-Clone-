789203c69816f29fc8569fce222817de00e47c5b
Dennis Gobbi <dennis.gobbi@edu.unito.it>
2022-12-03 12:51:38 +0100

    util js now is much smaller

--- js/util.js
+++ js/util.js
@@ -1,5 +1,7 @@
-function redirectLogin() { window.location.href = 'login.html'; }
-function redirectRegistrati() { window.location.href = 'registrati.html'; }
+/**
+JS File with common functions available through all the pages
+Mainly navbar related js functions
+*/
 
 async function sendApiRequest(mal_id, number_div = -1) {
   // Here we define our query as a multi-line string
@@ -138,159 +140,7 @@ function randomAnime() {
   })
 }
 
-function appendSelectedAnime() {
-  let id = event.target.id;
-  let title = event.target.innerHTML;
-  let anime = `<div class="anime" id="${id}">${title}</div>`;
-  $("#selected-anime").append(anime);
-}
 
-function goesToAnimePage($param) {
-  window.location.href = "anime.php?id=" + $param;
-}
-
-
-/**
- * Api:animepage
- */
-function deleteEntry(id_user, mal_id) {
-  $.ajax({
-    url: "api/animepage/deleteEntry.php",
-    type: "POST",
-    data: { id_user: id_user, mal_id: mal_id },
-    dataType: "json",
-    success: function (response) {
-      $("#" + mal_id).empty();
-      //delete the entry from the table
-
-    }
-  });
-}
-
-/**
- * Api:anime
- * @param {*} mal_id id of myanimelist entry in the database
- * @param {*} id_user id of the user in the user table
- */
-function addToList(mal_id, id_user) {
-  $.ajax({
-    url: "api/anime/addList.php",
-    type: "POST",
-    data: { mal_id: mal_id, id_user: id_user },
-    dataType: "json",
-    success: function (response) {
-      console.log(response);
-      if (response == "success") {
-        $("#btnAddList").html("Already added to your anime page");
-        $("#btnAddList").attr("disabled", true);
-      } else {
-        $("#login-error").html(response);
-      }
-    }
-  })
-}
-/**
- * Api:anime
- * @param {Integer} mal_id 
- * @param {Integer} id_user 
- */
-function postReview(mal_id, id_user) {
-  let review = $("#review").val();
-  console.log(review);
-  $.ajax({
-    url: "api/anime/postReview.php",
-    type: "POST",
-    data: { mal_id: mal_id, id_user: id_user, review: review },
-    dataType: "json",
-    success: function (response) {
-      console.log(response);
-      if (response == "success") {
-        location.reload();
-        // $("#review").val("");
-        // $("#review-error").html("Review posted!");
-      } else {
-        $("#review-error").html(response);
-      }
-    }
-  })
-}
-/**
- * Api:anime
- * @param {Integer} mal_id
- * @param {Integer} id_user
- */
-function deleteReview(id_review) {
-  $.ajax({
-    url: "api/anime/deleteReview.php",
-    type: "POST",
-    data: { id_review: id_review },
-    dataType: "json",
-    success: function (response) {
-      console.log(response);
-      if (response == "success") {
-        location.reload();
-      } else {
-        $("#review-error").html(response);
-      }
-    }
-  })
-}
-/**
- * Api:user
- */
-function checkLogin() {
-
-  let username = $("#username").val();
-  let password = $("#pw").val();
-  $.ajax({
-    url: "api/user/login.php",
-    type: "POST",
-    data: { username: username, password: password },
-    dataType: "json",
-    success: function (response) {
-      console.log(response);
-      if (response == "success") {
-        window.location.href = "index.php";
-      } else {
-        $("#login-error").html(response);
-      }
-    }
-  })
-}
-
-/**
- * Api:user
- */
-function checkRegistrati() {
-  let username = $("#username").val();
-  let password = $("#pw").val();
-
-  //check if username respects the regex [A-Za-z][A-Za-z0-9]{5,15}
-  let regex = /^[A-Za-z][A-Za-z0-9]{5,15}$/;
-  if (!regex.test(username)) {
-    $("#login-error").html("Username must be between 6 and 16 characters and can only contain letters and numbers and must start with a letter");
-    return;
-  } else {
-    // $("#login-error").html("Success");
-  }
-  $.ajax({
-    url: "api/user/registrati.php",
-    type: "POST",
-    data: { username: username, password: password },
-    dataType: "json",
-    success: function (response) {
-      console.log(response);
-      if (response == "success") {
-        window.location.href = "index.php";
-      } else {
-        $("#login-error").html(response);
-      }
-    }
-  })
-}
-/**
- * Api:user
- */
 function logout() {
   $.ajax({
     url: "api/user/logout.php",
@@ -300,4 +150,15 @@ function logout() {
       window.location.href = "index.php";
     }
   })
+
 }
+// function appendSelectedAnime() {
+//   let id = event.target.id;
+//   let title = event.target.innerHTML;
+//   let anime = `<div class="anime" id="${id}">${title}</div>`;
+//   $("#selected-anime").append(anime);
+// }
+
+// function goesToAnimePage($param) {
+//   window.location.href = "anime.php?id=" + $param;
+// }
