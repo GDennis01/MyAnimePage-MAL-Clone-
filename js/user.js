fc8e47af59a6fc7717749a4cbeb0414da88cbf32
Dennis Gobbi <dennis.gobbi@edu.unito.it>
2023-01-12 22:23:08 +0100

    shake on error

--- js/user.js
+++ js/user.js
@@ -1,6 +1,14 @@
 /*JS File with user-related functions, such as login,sign-up etc*/
 function redirectRegistrati() { window.location.href = 'registrati.html'; }
 function redirectLogin() { window.location.href = 'login.html'; }
+function setKeyframe(id) {
+  console.log("setKeyframe");
+  $("#" + id).addClass("shake");
+
+  $("#" + id).on("animationend", function () {
+    $("#" + id).removeClass("shake");
+  });
+}
 function checkLogin() {
 
   let username = $("#username").val();
@@ -16,6 +24,11 @@ function checkLogin() {
         window.location.href = "index.php";
       } else {
         $("#login-error").html(response);
+        setKeyframe("login");
+        //add .shake class to trigger keyframe
+
+        //remove .shake class after 1s
+
       }
     }
   })
@@ -28,6 +41,7 @@ function checkRegistrati() {
   let regex = /^[A-Za-z][A-Za-z0-9]{5,15}$/;
   if (!regex.test(username)) {
     $("#login-error").html("Username must be between 6 and 16 characters and can only contain letters and numbers and must start with a letter");
+    setKeyframe("registrati");
     return;
   } else {
     // $("#login-error").html("Success");
@@ -43,6 +57,7 @@ function checkRegistrati() {
         window.location.href = "index.php";
       } else {
         $("#login-error").html(response);
+        setKeyframe("registrati");
       }
     }
   })
